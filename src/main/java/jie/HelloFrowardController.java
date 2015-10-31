e737dc0a7edc8132936eaf296bad5b7eb9f6f6ce
jie.liu <jie.liu@rea-group.com>
2015-10-31 12:04:02 +0800

    java urlrewrite porject

---
+++ src/main/java/jie/HelloFrowardController.java
@@ -0,0 +1,17 @@
+package jie;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloFrowardController extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        out.println("hello, this is hello forward.");
+    }
+}
